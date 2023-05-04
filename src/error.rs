7728b2ec406264b3e74d7ac0915b81e59f7363c2
defnot001 <marc.miller994@gmail.com>
2023-05-04 09:37:20 +0200

    added parsing Error and docs

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,9 @@
+use std::fmt::{Display, Formatter};
 use thiserror::Error;
 
+/// `Error` type for this crate.
+///
+/// This is a simple `enum` that wraps the `std::io::Error` and `nbt::Error` types.
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("IO error: {0}")]
@@ -10,3 +14,13 @@ pub enum Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+/// `Error` type for parsing.
+#[derive(Debug, Error)]
+pub struct ParseError;
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Parse error")
+    }
+}
